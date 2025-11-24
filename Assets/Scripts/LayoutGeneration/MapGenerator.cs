7ff8af90d20cd0a7dace09ff85ff9e78c97e19ea
Hellwyn <elvinkauffmann@gmail.com>
2025-11-24 03:46:56 +0100

    Large rooms trying algorythm

--- Assets/Scripts/LayoutGeneration/MapGenerator.cs
+++ Assets/Scripts/LayoutGeneration/MapGenerator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,27 +13,68 @@ public class MapGenerator : MonoBehaviour
     private int maxRooms;
     private List<int> endRooms;
 
+    private int startIndex;
     private int bossRoomIndex;
     private int healRoomIndex;
     private int forgeRoomIndex;
-    private int lightRoomIndex;
+    private int itemRoomIndex;
 
+    private List<int> bigRoomIndexes;
     public Cell cellPrefab;
     private float cellSize;
     private Queue<int> cellQueue;
     private List<Cell> spawnedCells;
 
     [Header("Sprite references")]
-    [SerializeField] private Sprite light;
+    [SerializeField] private Sprite start;
+    [SerializeField] private Sprite item;
     [SerializeField] private Sprite forge;
     [SerializeField] private Sprite heal;
     [SerializeField] private Sprite boss;
+
+    [Header("Room Variations")]
+    [SerializeField] private Sprite largeRoom;
+    [SerializeField] private Sprite LRoom;
+    [SerializeField] private Sprite verticalRoom;
+    [SerializeField] private Sprite horizontalRoom;
+
+    private static readonly List<int[]> roomShapes = new()
+    {
+        new int[]{-1 },
+        new int[]{1 },
+
+        new int[]{10 },
+        new int[]{-10 },
+
+        new int[]{1,10},
+        new int[]{1,11},
+        new int[]{10,11},
+
+        new int[]{9,10},
+        new int[]{-1,9},
+        new int[]{-1,10},
+
+        new int[]{1,-10},
+        new int[]{1,-9},
+        new int[]{-9,-10},
+
+        new int[]{-1,-10},
+        new int[]{-1,-11},
+        new int[]{-10,-11},
+
+        new int[]{1,10,11},
+        new int[]{1,-9,-10},
+        new int[]{-1,9,10},
+        new int[]{-1,-10,-11}
+    };
+
     void Start()
     {
         minRooms = 7;
-        maxRooms = 12;
+        maxRooms = 15;
         cellSize = 0.5f;
         spawnedCells = new List<Cell>();
+        startIndex = 75;
 
         SetupFloor();
     }
@@ -56,8 +98,9 @@ public class MapGenerator : MonoBehaviour
         floorPlanCount = default;
         cellQueue = new Queue<int>();
         endRooms = new List<int>();
+        bigRoomIndexes = new List<int>();
 
-        VisitCell(75);
+        VisitCell(startIndex);
         GenerateFloor();
     }
 
@@ -84,8 +127,15 @@ public class MapGenerator : MonoBehaviour
             return;
         }
 
+        CleanEndRoomsList();
+
         SetupSpecialRooms();
         // UpdateSpecialRoomVisuals();
+    }
+
+    void CleanEndRoomsList()
+    {
+        endRooms.RemoveAll(item => bigRoomIndexes.Contains(item) || GetNeighbourCount(item) > 1);
     }
 
     void SetupSpecialRooms()
@@ -96,11 +146,11 @@ public class MapGenerator : MonoBehaviour
             endRooms.RemoveAt(endRooms.Count - 1);
         }
 
-        lightRoomIndex = RandomEndRoom();
+        itemRoomIndex = RandomEndRoom();
         forgeRoomIndex = RandomEndRoom();
         healRoomIndex = PickHealRoom();
 
-        if (lightRoomIndex == -1 || forgeRoomIndex == -1 || healRoomIndex == -1 || bossRoomIndex == -1)
+        if (itemRoomIndex == -1 || forgeRoomIndex == -1 || healRoomIndex == -1 || bossRoomIndex == -1)
         {
             SetupFloor();
             return;
@@ -116,8 +166,9 @@ public class MapGenerator : MonoBehaviour
         {
             var sprite = cell.index switch
             {
+                var index when index == startIndex => start,
                 var index when index == bossRoomIndex => boss,
-                var index when index == lightRoomIndex => light,
+                var index when index == itemRoomIndex => item,
                 var index when index == forgeRoomIndex => forge,
                 var index when index == healRoomIndex => heal,
                 _ => null
@@ -140,25 +191,52 @@ public class MapGenerator : MonoBehaviour
 
     int PickHealRoom()
     {
-        for (int attempt = 0; attempt < 900; attempt++)
+        int[] specialRooms = new int[] { bossRoomIndex, itemRoomIndex, forgeRoomIndex };
+
+        for (int index = 0; index < floorPlan.Length; index++)
         {
-            int x = Mathf.FloorToInt(Random.Range(0f, 1f) * 9) + 1;
-            int y = Mathf.FloorToInt(Random.Range(0f, 1f) * 8) + 2;
-
-            int index = y * 10 + x;
-
             if (floorPlan[index] != 0)
                 continue;
-            if (bossRoomIndex == index - 1 || bossRoomIndex == index + 1 ||
-               bossRoomIndex == index - 10 || bossRoomIndex == index + 10)
+
+            List<int> neighbours = GetNeighboursIndex(index);
+            if (neighbours.Count != 1)
                 continue;
-            if (index - 1 < 0 || index + 1 > floorPlan.Length || index - 10 < 0 || index + 10 > floorPlan.Length)
+
+            int neighbour = neighbours[0];
+            if (specialRooms.Contains(neighbour))
                 continue;
-            int neighbours = GetNeighbourCount(index);
-            if (neighbours >= 3 || (attempt > 300 && neighbours >= 2) || (attempt > 600 && neighbours >= 1))
+
+            List<int> secondNeighbours = GetNeighboursIndex(neighbour);
+
+            bool touchesSpecial = secondNeighbours.Exists(n =>
+                specialRooms.Contains(n)
+            );
+
+            if (touchesSpecial)
                 return index;
         }
         return -1;
+    }
+
+    List<int> GetNeighboursIndex(int index)
+    {
+        List<int> result = new List<int>();
+
+        int x = index % 10;
+        int y = index / 10;
+
+        void AddIfValid(int idx)
+        {
+            if (idx >= 0 && idx < floorPlan.Length && floorPlan[idx] == 1)
+                result.Add(idx);
+        }
+
+        if (x > 0) AddIfValid(index - 1);
+        if (x < 9) AddIfValid(index + 1);
+        if (y > 0) AddIfValid(index - 10);
+        if (y < 9) AddIfValid(index + 10);
+
+        return result;
     }
 
     private int GetNeighbourCount(int index)
@@ -171,16 +249,31 @@ public class MapGenerator : MonoBehaviour
 
     private bool VisitCell(int index)
     {
+        int x = index % 10;
+        int y = index / 10;
+        float verticalProb = 1f - (y / 9f);
+        float centerProb = 1f - (Mathf.Abs(x - 5) / 5f);
+        float chance = verticalProb * centerProb;
+
         if (floorPlan[index] != 0 ||
             GetNeighbourCount(index) > 1 ||
             floorPlanCount > maxRooms ||
-            Random.value < 0.5f
-        )
+            Random.value > chance)
             return false;
+
+        if (Random.value < 0.3f && index != startIndex)
+        {
+            foreach (var shape in roomShapes.OrderBy(_ => Random.value))
+            {
+                if (TryPlaceRoom(index, shape))
+                    return true;
+            }
+        }
 
         cellQueue.Enqueue(index);
         floorPlan[index] = 1;
         floorPlanCount++;
+
         SpawnRoom(index);
         return true;
     }
@@ -194,6 +287,88 @@ public class MapGenerator : MonoBehaviour
         Cell newCell = Instantiate(cellPrefab, pos, Quaternion.identity);
         newCell.index = index;
         newCell.value = 1;
+
+        spawnedCells.Add(newCell);
+    }
+
+    private bool TryPlaceRoom(int originIndex, int[] offsets)
+    {
+        List<int> currentRoomIndexes = new List<int>() { originIndex };
+        foreach (int offset in offsets)
+        {
+            int checkedIndex = originIndex + offset;
+            if (checkedIndex - 10 < 0 || checkedIndex + 10 >= floorPlan.Length || floorPlan[checkedIndex] != 0)
+            {
+                return false;
+            }
+            if (checkedIndex == originIndex) continue;
+            if (checkedIndex % 10 == 0) continue;
+
+            currentRoomIndexes.Add(checkedIndex);
+        }
+
+        if (currentRoomIndexes.Count == 1) return false;
+
+        foreach (int index in currentRoomIndexes)
+        {
+            floorPlan[index] = 1;
+            floorPlanCount++;
+            // cellQueue.Enqueue(index);
+            bigRoomIndexes.Add(index);
+        }
+
+        SpawnLargeRoom(currentRoomIndexes);
+
+        return true;
+    }
+
+    private void SpawnLargeRoom(List<int> largeRoomIndexes)
+    {
+        Cell newCell = null;
+
+        int combinedX = default;
+        int combinedY = default;
+        float offset = cellSize / 2f;
+
+        for (int i = 0; i < largeRoomIndexes.Count; i++)
+        {
+            int index = largeRoomIndexes[i];
+            int x = index % 10;
+            int y = index / 10;
+
+            combinedX += x;
+            combinedY += y;
+        }
+
+        if (largeRoomIndexes.Count == 4)
+        {
+            Vector2 position = new Vector2((combinedX / 4f) * cellSize + offset, -(combinedY / 4f) * cellSize - offset);
+
+            newCell = Instantiate(cellPrefab, position, Quaternion.identity);
+            newCell.SetRoomSprite(largeRoom);
+        }
+        else if (largeRoomIndexes.Count == 3)
+        {
+            Vector2 position = new Vector2((combinedX / 3f) * cellSize, -(combinedY / 3f) * cellSize);
+            newCell = Instantiate(cellPrefab, position, Quaternion.identity);
+            newCell.SetRoomSprite(LRoom);
+            newCell.RotateCell(largeRoomIndexes);
+        }
+        else if (largeRoomIndexes.Count == 2)
+        {
+            if (largeRoomIndexes[0] + 10 == largeRoomIndexes[1] || largeRoomIndexes[0] - 10 == largeRoomIndexes[1])
+            {
+                Vector2 position = new Vector2((combinedX / 2f) * cellSize + offset, -(combinedY / 2f) * cellSize - offset);
+                newCell = Instantiate(cellPrefab, position, Quaternion.identity);
+                newCell.SetRoomSprite(verticalRoom);
+            }
+            else if (largeRoomIndexes[0] + 1 == largeRoomIndexes[1] || largeRoomIndexes[0] - 1 == largeRoomIndexes[1])
+            {
+                Vector2 position = new Vector2((combinedX / 2f) * cellSize + offset, -(combinedY / 2f) * cellSize - offset);
+                newCell = Instantiate(cellPrefab, position, Quaternion.identity);
+                newCell.SetRoomSprite(horizontalRoom);
+            }
+        }
 
         spawnedCells.Add(newCell);
     }
